116a51fae85917b0490bebd45f3d2325acf094a9
M.Rami Alshaar <rshaar559@gmail.com>
2026-04-22 14:05:29 +0300

    updated mail view text-alignment

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,7 @@ import RightPanel from "./components/RightPanel";
 export default function DashboardPage() {
     return (
         <main className="flex flex-col h-fit w-screen overflow-hidden gap-12 p-4">
-            <div className="flex">
+            <div className="flex flex-row-reverse text-right">
                 {/* Mail List */}
                 <div className="w-[30%] bg-white rounded-xl border">
                     <MailList />
